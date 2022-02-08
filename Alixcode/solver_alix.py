9c0b892518b0df624bbff232d6b957ae2a7045e7
Alix Leroy <A.leroy@sms.ed.ac.uk>
2022-02-08 16:28:31 +0000

    alixs work

--- Alixcode/solver_alix.py
+++ Alixcode/solver_alix.py
@@ -5,6 +5,72 @@ from vedo.dolfin import plot, Latex, clear, histogram
 import matplotlib.pyplot as plt
 import pickle 
 import time
+
+
+def solver_all(nx,ny,tau,alpha, epsilon, num_steps, T):
+
+  # Create mesh and define function space
+  mesh = RectangleMesh(Point(-1, -1), Point(1, 1), nx, ny)
+  V = FunctionSpace(mesh, "P", 1)
+  
+  # Define boundary condition
+  tol = 1E-14
+  u_D = Expression('near(x[0], 1, tol) ? pow(1-x[1],4)*exp(-t*tau):0', degree=4, tol=tol,tau=tau, t=0)
+
+  def boundary(x, on_boundary):
+        return on_boundary
+
+  bc = DirichletBC(V, u_D, boundary)
+
+  # Define initial value
+  u_n = project(u_D, V)
+
+  # Define variational problem
+  u = TrialFunction(V)
+  v = TestFunction(V)
+  f = Constant(0.0)
+  
+  dt = 1 / num_steps # time step size
+  w = Expression(('exp(alpha)*2*x[1]*(1-x[0]*x[0])', 'exp(alpha)*-2*x[0]*(1-x[1]*x[1])'),alpha = alpha,degree=3)  #define the wind
+
+  F = u*v*dx + epsilon*dt*dot(grad(u), grad(v))*dx + dt*dot(w, grad(u))*v*dx - (u_n + dt*f)*v*dx
+  
+  a, L = lhs(F), rhs(F)
+
+  # Time-stepping
+  t = 0
+
+  # Create VTK file for saving solution
+  # namefile = str(alpha)+"-alpha.pvd"
+  # vtkfile = File("Alixcode/alixswork/"+namefile)
+  
+
+  # list to store t and u
+  u = Function(V)
+  t_u_list = []
+
+  for n in range(int(T)*num_steps):
+
+      # Update current time
+      t += dt
+      u_D.t = t #update the time in the boundary condition
+
+
+      # Compute solution
+      solve(a == L, u, bc)
+
+
+      
+      # Compute u at the vertices and add them to the list
+      u_approx = u.compute_vertex_values(mesh)
+      t_u_list.append(u_approx)
+
+      # Update previous solution
+      u_n.assign(u)
+  
+  return(np.array(t_u_list))
+
+
 def solver_para(nx,ny,tau):
   # Create mesh and define function space
   mesh = RectangleMesh(Point(-1, -1), Point(1, 1), nx, ny)
