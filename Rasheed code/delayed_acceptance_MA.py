bfddb3232584d5cab3366002b5775eb61b4e3979
Rasheed19 <46923313+Rasheed19@users.noreply.github.com>
2022-02-21 14:31:48 +0000

    Update delayed_acceptance_MA.py

--- Rasheed code/delayed_acceptance_MA.py
+++ Rasheed code/delayed_acceptance_MA.py
@@ -14,14 +14,20 @@ warnings.filterwarnings('ignore')
 set_log_active(False)
 
 def prior(w):
-    #return np.exp(-0.5 * w**2)
-    return np.exp(-w)
+    return np.exp(-0.5 * w**2)
+    #return np.exp(-w)
 
-def target(w):
-    return (1 / (np.sqrt(2*np.pi))) * np.exp(-0.5 * w**2)
+def target(w, sigma):
+    return (1 / (np.sqrt(2*np.pi))*sigma) * np.exp(-0.5/sigma**2 * w**2)
 
 def likelihood(A, y, sigma):
-    return (1 / (np.sqrt(2*np.pi))) * np.exp((-0.5/sigma**2) * np.linalg.norm(y-A)**2)  # the parameter space is just a subset of the real line
+    return (1 / (np.sqrt(2*np.pi))*sigma) * np.exp((-0.5/sigma**2) * np.linalg.norm(y-A)**2)  # the parameter space is just a subset of the real line
+
+def cheap_likelihood(A, y, sigma):
+    y = np.linalg.norm(y, axis=1)
+    A = np.linalg.norm(A, axis=1)
+    return (1 / (np.sqrt(2*np.pi))*sigma) * np.exp((-0.5/sigma**2) * np.linalg.norm(y-A)**2)  # the parameter space is just a subset of the real line
+
 
 def proposal(X_prime, X_i, sigma):
     return np.exp(-0.5*(X_prime-X_i)**2/sigma**2) / (np.sqrt(2*np.pi)*sigma)
@@ -58,7 +64,7 @@ def mcmc_diagnosis_plots(mcmc_chain, mcmc_acceptance_ratio, burn_in_period, lag,
     ax[0,0].set_title(r'Trace plot of $\alpha$ before and after the burn-in period')
     ax[0,0].legend()
 
-    sns.histplot(alpha_dt, kde=True, ax=ax[0,1])
+    sns.histplot(alpha_dt, kde=True, stat="probability", ax=ax[0,1])
     ax[0,1].set_xlabel(r'$ \alpha $')
     ax[0,1].set_title(r'Distribution plot of $ \alpha $ after the burn-in period')
     
@@ -77,17 +83,17 @@ def mcmc_diagnosis_plots(mcmc_chain, mcmc_acceptance_ratio, burn_in_period, lag,
     plt.show()
 
 
-def delayed_acceptance(true_parameter, alpha_0, sigma, num_itr):
+def delayed_acceptance(true_parameter, alpha_0, sigma_p, sigma_l, num_itr):
 
     tol = 1e-14  # threshold to compare the current proposal and previous proposal
-    tau = 1/100
+    tau = 1/10
 
     # initialize a list to monitor acceptance ratio
     accepted_state_list = []
     acceptance_ratio_list = []
 
     # generate data
-    y = generate_noise_data(sigma=0.02, parameter=true_parameter, tau=tau)
+    y = generate_noise_data(sigma=0.01, parameter=true_parameter, tau=tau)
     
     start_time = time.time()   
 
@@ -100,20 +106,21 @@ def delayed_acceptance(true_parameter, alpha_0, sigma, num_itr):
     
     # initialize the mesh, function space and source term
     mesh, V, u, v, f = fenics_essentials_exponential(nx=10, ny=10, k=1, f_val=0.0)
+    cheap_mesh, cheap_V, cheap_u, cheap_v, cheap_f = fenics_essentials_exponential(nx=5, ny=5, k=1, f_val=0.0)
 
     A_0 = pred_exponential_time_dg(alpha_0, tau, mesh, V, u, v, f, epsilon=1/200, num_steps=100, T=5.0)
-    #A_0 = exponential_time_double_glazing(parameter=alpha_0, tau=tau)
+    cheap_A_0 = pred_exponential_time_dg(alpha_0, tau, cheap_mesh, cheap_V, cheap_u, cheap_v, cheap_f, epsilon=1/200, num_steps=100, T=5.0)
+
     i = 0
     
     while num_itr > i:
 
-        alpha_prime = alpha_0 + sigma*np.random.normal(0, 1)   # random walk proposal
-
-        A = pred_exponential_time_dg(alpha_prime, tau, mesh, V, u, v, f, epsilon=1/200, num_steps=100, T=5.0)
-        #A = exponential_time_double_glazing(parameter=alpha_prime, tau=tau)
+        alpha_prime = alpha_0 + sigma_p*np.random.normal(0, 1)   # random walk proposal
+        
+        cheap_A = pred_exponential_time_dg(alpha_prime, tau, cheap_mesh, cheap_V, cheap_u, cheap_v, cheap_f, epsilon=1/200, num_steps=100, T=5.0)
 
         # calculates the accetance ratio for the first rejection criterion
-        acceptance_ratio = (likelihood(A, y, sigma) * prior(alpha_prime)) / (likelihood(A_0, y, sigma) * prior(alpha_0))
+        acceptance_ratio = (cheap_likelihood(cheap_A, y, sigma_l) * prior(alpha_prime)) / (cheap_likelihood(cheap_A_0, y, sigma_l) * prior(alpha_0))
         
         # calculates the acceptance probability
         g = min(1, acceptance_ratio)
@@ -122,23 +129,27 @@ def delayed_acceptance(true_parameter, alpha_0, sigma, num_itr):
         if g < np.random.uniform(0, 1):
             alpha_prime = alpha_0
 
-        # calculates the accetance ratio for the second rejection criterion
-        acceptance_ratio = (target(alpha_prime) * proposal(alpha_0, alpha_prime, sigma)) / (target(alpha_0) * proposal(alpha_prime, alpha_0, sigma)) 
-        rho = min(1, acceptance_ratio)  
-
         if abs(alpha_prime - alpha_0) > tol:   # will only proceed to the next stage if the current proposal is different from the previous one
+
+            A = pred_exponential_time_dg(alpha_prime, tau, mesh, V, u, v, f, epsilon=1/200, num_steps=100, T=5.0)
+            
+            # calculates the acceptance ratio for the second rejection criterion
+            acceptance_ratio = (likelihood(A, y, sigma_l) * prior(alpha_prime)) / (likelihood(A_0, y, sigma_l) * prior(alpha_0)) 
+            rho = min(1, acceptance_ratio)  
+
+            A_0 = A
+
             # acceptance decision
-            if rho > np.random.uniform(0, 1):
-                alpha.append(alpha_prime)
+            if rho >= np.random.uniform(0, 1):
                 accepted_state_list.append(alpha_prime)
-            else:
-                alpha.append(alpha_0)
+                alpha_0 = alpha_prime
+
+            alpha.append(alpha_0)
         
         # update the acceptance ratio list 
         acceptance_ratio_list.append(len(accepted_state_list)/len(alpha))
 
-        A_0 = A
-        alpha_0 = alpha_prime
+        cheap_A_0 = cheap_A
         i += 1
 
     end_time = time.time()
