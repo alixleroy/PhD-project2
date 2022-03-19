aa56bad56e6c5328a4644d385aa9cfd7aa88ddf0
Rasheed19 <46923313+Rasheed19@users.noreply.github.com>
2022-03-19 13:44:55 +0000

    Update KL_Divergence.py

--- Rasheed code/KL_Divergence.py
+++ Rasheed code/KL_Divergence.py
@@ -1,9 +1,4 @@
 import numpy as np
-from Variable_Time_Solver import *
-from Functions_Library import load_data, likelihood, prior, stretch_mesh
-from Time_stepping import *
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 def estimate_distribution(data,
                         t_list,
@@ -12,7 +7,7 @@ def estimate_distribution(data,
                         nx,
                         ny):
 
-    # initialize the mesh, function space and source term
+    # initialize the mesh, function space
     mesh, V, u, v = stretch_mesh(nx=nx, ny=ny)
     estimation_list = []
 
@@ -59,51 +54,8 @@ def kl_divergence_measure(data,
                         nx,
                         ny)
 
-    # the relative entropy from p to q
+    # the relative entropy from q to p
     kl = np.sum(p * np.log2(p / q))
 
     return kl
 
-# Load data 
-data = load_data('master_data_32_project')
-
-# Find t_list
-t_list = find_t_list(dt_min=1e-7,
-                dt_max=0.1,
-                tau=1/10,
-                reg=10,
-                num_steps=60
-                )
-
-# Organize the data 
-organized_data = time_organiser(y_data = data,
-                   dt_min=1e-7,
-                   dt_max=0.1,
-                   tau=1/10,
-                   reg=10,
-                   num_steps=60,
-                   data_res=0.00001
-                   )
-
-alpha_list = np.load('var_delayed_MCMC.npy')
-
-kl = kl_divergence_measure(data = organized_data,
-                        t_list = t_list,
-                        alpha_list = alpha_list,
-                        sigma_l = 0.2,
-                        nx=32,
-                        ny=32,
-                        nbins=100)
-
-print(kl)
-
-'''
-
-print(estimate_distribution(organized_data,
-                    t_list,
-                    alpha_list[1990:],
-                    0.2,
-                    32,
-                    32)
-)
-'''
