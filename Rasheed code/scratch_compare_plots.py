638e1a58cc09cfe213c25a76d2cae3f138d68015
Rasheed19 <46923313+Rasheed19@users.noreply.github.com>
2022-03-22 10:59:51 +0000

    Update scratch_compare_plots.py

--- Rasheed code/scratch_compare_plots.py
+++ Rasheed code/scratch_compare_plots.py
@@ -2,13 +2,13 @@ import numpy as np
 from Functions_Library import compare_diagnosis_plots, load_data
 
 
-chain_data = [(np.load('var_metropolis.npy'), load_data('alpha_list_RWMH')),
-            (np.load('var_delayed_MCMC.npy'), load_data('alpha_list_DA')),
-            (np.load('var_gamma_nonreversible_MCMC.npy'), load_data('alpha_list_IRWMH'))]
+chain_data = [(np.load('chains/var_metropolis.npy'), load_data('chains/alpha_list_RWMH')),
+            (np.load('chains/var_delayed_MCMC.npy'), load_data('chains/alpha_list_DA')),
+            (np.load('chains/var_gamma_nonreversible_MCMC.npy'), load_data('chains/alpha_list_IRWMH'))]
 
-acceptance_ratio_data = [(np.load('var_metropolis_accept_ratio.npy'), load_data('A_list_RWMH')),
-                        (np.load('var_delayed_MCMC_accept_ratio.npy'), load_data('A_list_DA')),
-                        (np.load('var_gamma_nonreversible_MCMC_accept_ratio.npy'), load_data('A_list_IRWMH'))]
+acceptance_ratio_data = [(np.load('chains/var_metropolis_accept_ratio.npy'), load_data('chains/A_list_RWMH')),
+                        (np.load('chains/var_delayed_MCMC_accept_ratio.npy'), load_data('chains/A_list_DA')),
+                        (np.load('chains/var_gamma_nonreversible_MCMC_accept_ratio.npy'), load_data('chains/A_list_IRWMH'))]
 
 labels = [('RWMH: Cell averaging', 'RWMH: Interpolation'), ('Delayed acceptance: Cell averaging', 'Delayed acceptance: Interpolation'),
           ('Irreversible RWMH: Cell averaging', 'Irreversible RWMH: Interpolation')]
